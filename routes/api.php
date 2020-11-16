b221deb0d2b985aff9aadec1dde92893b63cfbdf
Yoharu <jonathas_a.a@hotmail.com>
2020-11-15 22:49:21 -0300

    Cria a rota de vídeo Feita a criação da rota de vídeo e adicionado o retorno do file.

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Storage;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/video/{id}', function($id){
+    $url = Storage::path("Videos/$id.mp4");
+    return response()->file($url);
 });
