c858132e6fc9160b85b12c1f3832027af1809fbe
João Apostulo <japostulo@gmail.com>
2020-11-16 23:59:29 -0300

    Altera a rota video

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('video')->group(function () {
+Route::prefix('videos')->group(function () {
     Route::get('/{type}/{id}', [VideoController::class, 'show']);
 });
