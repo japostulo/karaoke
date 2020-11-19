69cb3afae872e360a4c5e54ae080f4a764db2e71
João Apostulo <japostulo@gmail.com>
2020-11-19 07:41:26 -0300

    Cria MVC da tabela Artist Realizado criação do model e configuração inicial  $fillable e $table

---
+++ app/Models/Artist.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Artist extends Model
+{
+    protected $fillable = [
+        'id', 'name',
+    ];
+
+    protected $table = [
+        'artists'
+    ];
+
+}
