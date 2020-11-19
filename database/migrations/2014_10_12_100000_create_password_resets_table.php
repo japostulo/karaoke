245dd0932051ab0790e5115f520f68c2afe2a3d1
João Apostulo <japostulo@gmail.com>
2020-11-19 07:42:51 -0300

    Estrutura inicial laravel com demilitação de 40 caracteres

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -14,8 +14,8 @@ class CreatePasswordResetsTable extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
+            $table->string('email', 40)->index();
+            $table->string('token', 90);
             $table->timestamp('created_at')->nullable();
         });
     }
